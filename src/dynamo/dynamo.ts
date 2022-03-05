8fecbc19ce1f994267ba3ec04fd6b3e1e61af0a0
Jung Woo <michaelwoo921@gmail.com>
2022-03-04 23:40:45 -0800

    push to git

--- src/dynamo/dynamo.ts
+++ src/dynamo/dynamo.ts
@@ -2,7 +2,9 @@ import * as AWS from 'aws-sdk';
 
 const docClient = new AWS.DynamoDB.DocumentClient({
   region: 'us-west-2',
-  endpoint: 'http://dynamodb.us-west-2.amazonaws.com'
-})
+  endpoint: 'http://dynamodb.us-west-2.amazonaws.com',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 export default docClient;
